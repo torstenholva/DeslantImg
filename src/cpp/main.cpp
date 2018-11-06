47dd553317245bd6a38792dc148dad6e2bc50401
Torsten Holva <torstenholva@users.noreply.github.com>
2018-11-06 15:26:32 +0200

    Take multiple arguments of image names (png) from one hierarchy level above and output to 02_deslanted on same level.

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -9,30 +9,51 @@
 #endif
 #include <opencv2/imgcodecs.hpp>
 #include <iostream>
+#include <exception>    // std::exception
 
 
-int main() 
+int main(int argc, char* argv[])
 {
-	// load input image
-	const cv::Mat img1 = cv::imread("data/test1.png", cv::IMREAD_GRAYSCALE);
-	const cv::Mat img2 = cv::imread("data/test2.png", cv::IMREAD_GRAYSCALE);
-	
-	
-#ifdef USE_GPU 
-	// deslant on GPU
-	htr::CLWrapper clWrapper; // setup OpenCL, the same instance should be used for all following calls to deslantImg
-	const cv::Mat res1 = htr::deslantImg(img1, 255, clWrapper);
-	const cv::Mat res2 = htr::deslantImg(img2, 255, clWrapper);
-#else
-	// deslant on CPU
-	cv::Mat res1 = htr::deslantImg(img1, 255);
-	cv::Mat res2 = htr::deslantImg(img2, 255);
-#endif
+	// We expect 2 arguments: the program name, the source path and the destination path
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " <image_name>.png" << std::endl;
+		return 1;
+	}
 
-	// write results to file
-	cv::imwrite("out1.png", res1);
-	cv::imwrite("out2.png", res2);
+	std::vector<std::string> sources;
+	std::string destination;
+	for (int i = 1; i < argc; ++i) {
+		if (std::string(argv[i]) == "--destination") {
+			if (i + 1 < argc) { // Make sure we aren't at the and of argv!
+				destination = argv[i++]; // increment 'i' so we don't get the argument as the next argv[i].
+			}
+			else { // Uh-oh, there was no argument to the destination opiton.
+				std::cerr << "--destination option requires one argument." << std::endl;
+				return 1;
+			}
+		}
+		else {
+			sources.push_back(argv[i]);
+		}
+	}
+
+	std::cout << "../01_segmented/ => ../02_deslanted/" << std::endl;
+
+	try {
+		for (std::vector<std::string>::const_iterator itr = sources.begin();
+			 itr != sources.end();
+			 ++itr)
+		{
+			std::cout << *itr + ".png" << std::endl;
+			const cv::Mat img = cv::imread("../01_segmented/" + *itr + ".png", cv::IMREAD_GRAYSCALE);
+			cv::Mat res = htr::deslantImg(img, 255);
+			cv::imwrite("../02_deslanted/" + *itr + ".png", res);
+		}
+	}
+	catch(const std::exception& e) {
+		std::cerr << "Standard exception: " << e.what()  << std::endl;
+		return 1;
+	}
 
 	return 0;
 }
-
