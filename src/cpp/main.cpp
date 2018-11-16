8ca88bc345f09450c9b8b6738ff235770edd1c7e
Torsten Holva <torstenholva@users.noreply.github.com>
2018-11-16 07:17:25 +0200

    Command line example: sudo ./DeslantImg --inpath 00_in_sentence/ --midpath 0.png/ 0 1 2 3 4 5 6 > ../00_in_sentence/0.png => ../02_deslanted/0.png > ../00_in_sentence/1.png => ../02_deslanted/1.png > ../00_in_sentence/2.png => ../02_deslanted/2.png > ../00_in_sentence/3.png => ../02_deslanted/3.png > ../00_in_sentence/4.png => ../02_deslanted/4.png > ../00_in_sentence/5.png => ../02_deslanted/5.png > ../00_in_sentence/6.png => ../02_deslanted/6.png

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -24,17 +24,28 @@ int main(int argc, char* argv[])
   }
 
   std::vector<std::string> sources;
-  std::string midPath;
-  std::string inDir = "../01_segmented/";
+  std::string inPath = "";
+  std::string midPath = "";
+  std::string inDir = "../";
   std::string outDir = "../02_deslanted/";
+  
   for (int i = 1; i < argc; ++i) {
-    std::cout << std::string(midPath + " " + argv[i]) << std::endl;
-
-    if (std::string(argv[i]) == "--midpath") {
+    if (std::string(argv[i]) == "--inpath") {
+      if (i + 1 < argc) { // Make sure we aren't at the end of argv!
+        inPath = argv[++i]; // increment 'i' so we don't get the argument as the next argv[i].
+        // std::cout << std::string("inPath: " + inPath) << std::endl;
+      }
+      else { // Uh-oh, there was no argument to the options
+        std::cerr << "--inpath option requires one argument." << std::endl;
+        return 1;
+      }
+    }
+    else if (std::string(argv[i]) == "--midpath") {
       if (i + 1 < argc) { // Make sure we aren't at the and of argv!
         midPath = argv[++i]; // increment 'i' so we don't get the argument as the next argv[i].
+        // std::cout << std::string("midPath: " + midPath) << std::endl;
       }
-      else { // Uh-oh, there was no argument to the destination opiton.
+      else { // Uh-oh, there was no argument to the opiton.
         std::cerr << "--midpath option requires one argument." << std::endl;
         return 1;
       }
@@ -44,7 +55,8 @@ int main(int argc, char* argv[])
     }
   }
 
-  std::cout << "../01_segmented/ => ../02_deslanted/" << std::endl;
+  inDir = inDir + inPath;
+  // std::cout << inDir + " => ../02_deslanted/" << std::endl;
 
   try {
     if (stat(std::string(outDir + midPath).c_str(), &sb) != 0 && !S_ISDIR(sb.st_mode)) {
@@ -55,8 +67,7 @@ int main(int argc, char* argv[])
          itr != sources.end();
          ++itr)
       {
-        std::cout << inDir + midPath + *itr + ".png" << std::endl;
-        std::cout << outDir + midPath + *itr + ".png" << std::endl;
+        std::cout << inDir + midPath + *itr + ".png => " + outDir + midPath + *itr + ".png" << std::endl;
         const cv::Mat img = cv::imread(inDir  + midPath + *itr + ".png", cv::IMREAD_GRAYSCALE);
         cv::Mat res = htr::deslantImg(img, 255);
         cv::imwrite(outDir + midPath + *itr + ".png", res);
